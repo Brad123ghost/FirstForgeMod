8156e3b8f3ce1a24f4f093a251b9fa33c62066ba
Bradley Chung <rcn6795@autuni.ac.nz>
2023-06-20 02:31:47 +1200

    Created file

--- src/main/java/com/bradley/firstmod/init/BlockInit.java
+++ src/main/java/com/bradley/firstmod/init/BlockInit.java
@@ -1,4 +1,30 @@
 package com.bradley.firstmod.init;
 
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class BlockInit {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, "firstmod");
+
+    public static final RegistryObject<Block> CUSTOM_BLOCK = BLOCKS.register("custom_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.ANVIL)
+                    .mapColor(MapColor.COLOR_GREEN)
+                    .strength(5.0f, 17f)
+                    .instrument(NoteBlockInstrument.BANJO)
+                    .lightLevel(state -> 10)
+                    .requiresCorrectToolForDrops()
+                    .pushReaction(PushReaction.DESTROY)
+            ));
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
 }
